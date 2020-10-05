addf90bdf0c4c8932829b0020a3513893dde3050
edsongs <67446405+edsongs@users.noreply.github.com>
2020-10-05 15:31:19 -0300

    Update 0-base.php

--- configuracoes/config.d/0-base.php
+++ configuracoes/config.d/0-base.php
@@ -1,18 +1,11 @@
 <?php
 
-$site_name        = 'Mapa Cultural de Santa Catarina';
-$site_description = 'O Mapa Cultural de Santa Catarina é a plataforma livre, gratuita e colaborativa de mapeamento do Estado de Santa Catarina sobre cenário cultural catarinense. Ficou mais fácil se programar para conhecer as opções culturais que as cidades catarinenses oferecem: shows musicais, espetáculos teatrais, sessões de cinema, saraus, entre outras. Além de conferir a agenda de eventos, você também pode colaborar na gestão da cultura do estado: basta criar seu perfil de agente cultural. A partir deste cadastro, fica mais fácil participar dos editais e programas da Secretaria e também divulgar seus eventos, espaços ou projetos.';
-
 $base_domain   = @$_SERVER['HTTP_HOST'];
 $base_url      = 'http://' . $base_domain . '/';
 date_default_timezone_set('America/SaoPaulo');
 
 return array_merge($config,
     [
-        'app.useAssetsUrlCache' => false,
-        'app.siteName'          => \MapasCulturais\i::__($site_name),
-        'app.siteDescription'   => \MapasCulturais\i::__($site_description),
-
         /* to setup Saas Subsite theme */
 /*      'namespaces' => [
                         'MapasCulturais\Themes' => THEMES_PATH,
@@ -23,14 +16,5 @@ return array_merge($config,
 //        'themes.active' => 'Santa_Catarina',
         'base.assetUrl' => $base_url . 'assets/',
         'base.url'      => $base_url,
-
-        /* Habilitar configurações importantes da aplicação: [development, staging, production] */ 
-        'app.mode'                     => 'production',
-        'app.enabled.seals'            => false,
-        'app.enabled.apps'             => false,
-        'api.accessControlAllowOrigin' => '*',
-        'app.offline'                  => false,
-        'app.offlineUrl'               => '/offline',
-        'app.offlineBypassFunction'    => null
     ]
 );
