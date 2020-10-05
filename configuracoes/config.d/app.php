e85f8b147614f4722dde4ebf46f1bbc6dd6cdd9b
edsongs <67446405+edsongs@users.noreply.github.com>
2020-10-05 15:29:40 -0300

    Update app.php

--- configuracoes/config.d/app.php
+++ configuracoes/config.d/app.php
@@ -1,6 +1,12 @@
+<?php
+
+return array_merge($config,
+    [
         'app.log.hook'         => true,
         #'app.log.query'        => true,
         #'app.log.requestData'  => true,
         #'app.log.translations' => true,
         #'app.log.apiCache'     => true,
         #'app.log.path'         => '/dados/mapas/logs/',
+    ]
+);
