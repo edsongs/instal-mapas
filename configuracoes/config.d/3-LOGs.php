6a4f169ef29b9e0c3f87dc3ff4e76cfecdb005e3
edsongs <67446405+edsongs@users.noreply.github.com>
2020-10-05 15:28:50 -0300

    Update 3-LOGs.php

--- configuracoes/config.d/3-LOGs.php
+++ configuracoes/config.d/3-LOGs.php
@@ -7,11 +7,6 @@ return array_merge($config,
         'slim.log.enabled'     => true,
         'slim.log.level'       => \Slim\Log::DEBUG,
         'app.log.hook'         => true,
-        #'app.log.query'        => true,
-        #'app.log.requestData'  => true,
-        #'app.log.translations' => true,
-        #'app.log.apiCache'     => true,
-        #'app.log.path'         => '/dados/mapas/logs/',
         #'slim.log.writer'      => new \MapasCulturais\Loggers\File(function () {return 'slim.log'; }),
     ]
 );
