93daba2c93f0581294272e3a2a5e108bdf2397c2
edsongs <67446405+edsongs@users.noreply.github.com>
2020-10-05 15:28:07 -0300

    Create app.php

---
+++ configuracoes/config.d/app.php
@@ -0,0 +1,6 @@
+        'app.log.hook'         => true,
+        #'app.log.query'        => true,
+        #'app.log.requestData'  => true,
+        #'app.log.translations' => true,
+        #'app.log.apiCache'     => true,
+        #'app.log.path'         => '/dados/mapas/logs/',
